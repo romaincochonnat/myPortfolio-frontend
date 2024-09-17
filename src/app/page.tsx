d4318741a3ceb2dc3f34e58fc913307d33bdb8c7
Romain Cochonnat <romain.cochonnat@gmail.com>
2024-09-17 14:48:12 +0200

    change width of main container

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,7 @@ import {
 
 export default function Myportfolio() {
   return (
-    <main className=" w-[300px] lg:w-[1440px] lg:max-w-[1440px] flex flex-col items-center  ">
+    <main className=" w-full max-w-[480px] lg:w-[1440px] lg:max-w-[1440px] flex flex-col items-center  ">
       <Home />
       <Expertise />
       <Works />
